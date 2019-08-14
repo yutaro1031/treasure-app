666f92f22a1c66cf02a055024b5d557f05f4a2d5
yang-wei <yangweilim21@gmail.com>
2019-08-14 17:42:38 +0900

    ...

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,63 @@
-<template lang="pug">
-  .container Hello {{bundler}}
+<template>
+  <div v-if="user">
+  	<button v-on:click="login">Please login</button>;
+  </div>
+  <div v-else>
+    <div>{{message}}</div>
+    <p style="color:red;">{{errorMessage}}</p>
+    <button v-on:click="getPrivateMessage">
+      Get Private Message
+    </button>
+    <button v-on:click="logout">Logout</button>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import firebase from "./firebase";
+import { getPrivateMessage, getPublicMessage } from "./api";
 
 export default Vue.extend({
+
+  created() {
+  	firebase.auth().onAuthStateChanged(user => {
+  	  if (user) {
+  	    this.user = user ;
+  	  } else {
+  	    this.user = null
+  	  }
+  	});
+  },
   data() {
     return {
-      bundler: "Parcel"
-    };
+      user: null,
+      message: "",
+      errorMessage: ""
+    }
+  },
+  methods: {
+
+  	login: function() {
+  		firebase.login();
+  	},
+
+  	logout: function() {
+  		firebase.logout();
+  	},
+
+  	getPrivateMessage: function() {
+  		this.user
+  		  .getIdToken()
+  		  .then(token => {
+  		    return getPrivateMessage(token);
+  		  })
+  		  .then(resp => {
+  		    this.message = resp.message;
+  		  })
+  		  .catch(error => {
+  		    this.errorMessage = error.toString()
+  		  });
+  	}
   }
 });
 </script>
