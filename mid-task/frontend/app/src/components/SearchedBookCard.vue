812977b0dc596ca4dba2df5cb2726f60f69a9ac0
yutaro1031 <yutaro50@gmail.com>
2019-08-17 23:10:41 +0900

    Add func registBook

--- mid-task/frontend/app/src/components/SearchedBookCard.vue
+++ mid-task/frontend/app/src/components/SearchedBookCard.vue
@@ -3,12 +3,22 @@
     <v-img :src="book_info.largeImageUrl" height="170px" width="120px" class="mx-auto"> </v-img>
 
     <v-card-text>
-      <h3 class="card-text">{{ book_info['title'] }}</h3>
+      <div class="card-text">
+        <p>{{ book_info['title'] }}</p>
+        <span class="grey--text discription-link">
+          <a class="not-decolation" v-bind:href="book_info.itemUrl" target="_blank">
+            楽天市場で詳細をみる
+          </a>
+        </span>
+      </div>
     </v-card-text>
 
     <v-card-actions>
-      <v-btn class="mx-auto" color="info" v-bind:href="book_info.itemUrl" target="_blank">
-        楽天市場で詳細をみる
+      <v-btn v-if="!isRegistered" class="mx-auto" color="info" @click="registBook">
+        登録する
+      </v-btn>
+      <v-btn v-else class="mx-auto" color="blue-grey" disabled>
+        登録済み
       </v-btn>
     </v-card-actions>
   </v-card>
@@ -17,14 +27,43 @@
 <script>
 export default {
   props: ['book_info'],
-  // data() {},
-  created() {},
-  methods: {}
+  data() {
+    return {
+      isRegistered: false
+    };
+  },
+  methods: {
+    registBook() {
+      const url = process.env.VUE_APP_API_URL + '/books';
+      const bookInfo = this.$props.book_info;
+      const body = JSON.stringify({
+        name: bookInfo.title,
+        isbn: bookInfo.isbn,
+        image_url: bookInfo.largeImageUrl,
+        item_url: bookInfo.largeImageUrl
+      });
+      const headers = {
+        'Content-Type': 'application/json'
+      };
+      fetch(url, { method: 'POST', headers, body }).then((this.$data.isRegistered = true));
+    }
+  }
 };
 </script>
 
 <style>
+.discription-link {
+  display: block;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
 .card-text {
-  min-height: 50px;
+  position: relative;
+  min-height: 100px;
+}
+.not-decolation {
+  text-decoration: none;
+  color: grey;
 }
 </style>
